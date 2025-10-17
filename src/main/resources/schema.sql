97aa5f6a466199f506b4e018f5edfecfd6bd58b1
Fanani23 <pramudiasf23@gmail.com>
2025-10-17 16:40:41 +0700

    main push

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,8 +1,8 @@
 CREATE TABLE IF NOT EXISTS products (
-    id BIGSERIAL PRIMARY KEY,
-    title VARCHAR(255) NOT NULL,
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255),
+    price DECIMAL(19,2),
     handle VARCHAR(255) NOT NULL UNIQUE,
     vendor VARCHAR(255),
-    image TEXT,
-    price DECIMAL(10,2)
+    image CLOB
 );
